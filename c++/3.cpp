d209d62090cb2fcba265826329c1cdade7066bc6
Rekord <1324596506@qq.com>
2022-02-28 20:03:47 +0800

    feat: add 3 leetcode-28 solutions which use c++

--- c++/3.cpp
+++ c++/3.cpp
@@ -148,7 +148,7 @@ public:
         return nums.size() - res;
     }
 
-    int strStr(string haystack, string needle) {
+    int strStr1(string haystack, string needle) {
         int m = haystack.size(), n = needle.size();
         for (int i = 0; i <= m - n; i++) {
             int j = 0;
@@ -162,6 +162,49 @@ public:
             }
         }
         return -1;
+    }
+    int strStr2(string haystack, string needle) {
+        int m = haystack.size(), n = needle.size(), p = 0;
+        while (p + n - 1 < m) {
+            if (haystack.substr(p, n) == needle) {
+                return p;
+            }
+            while (p++ + n - 1 < m && haystack[p] != needle[0]);
+        }
+        return -1;
+    }
+    int strStr3(string haystack, string needle) {
+        int m = haystack.size(), n = needle.size();
+        if (!n) {
+            return 0;
+        }
+        vector<int> lps = kmpProcess(needle);
+        for (int i = 0, j = 0; i < m;) {
+            if (haystack[i] == needle[j]) { 
+                i++, j++;
+            }
+            if (j == n) {
+                return i - j;
+            }
+            if (i < m && haystack[i] != needle[j]) {
+                j ? j = lps[j - 1] : i++;
+            }
+        }
+        return -1;
+    }
+    vector<int> kmpProcess(string needle) {
+        int n = needle.size();
+        vector<int> lps(n, 0);
+        for (int i = 1, len = 0; i < n;) {
+            if (needle[i] == needle[len]) {
+                lps[i++] = ++len;
+            } else if (len) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
+            }
+        }
+        return lps;
     }
     int divide(int dividend, int divisor) {
         if (dividend == 0x80000000 && divisor == -1)return 0x7FFFFFFF;
