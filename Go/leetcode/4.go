69a92e26afe74e75a2a78199622151ac8b87ce82
rekord <1324596506@qq.com>
2022-03-02 14:56:28 +0800

    feat: add leetcode-36 solution which use go

--- Go/leetcode/4.go
+++ Go/leetcode/4.go
@@ -31,3 +31,26 @@ func search(nums []int, target int) int {
     } 
     return -1
 }
+func isValidSudoku(board [][]byte) bool {
+    rowbuf, colbuf, boxbuf := make([][]bool, 9), make([][]bool, 9), make([][]bool, 9)
+    for i := 0; i < 9; i++ {
+        rowbuf[i] = make([]bool, 9)
+        colbuf[i] = make([]bool, 9)
+        boxbuf[i] = make([]bool, 9)
+    }
+    // 遍历一次，添加缓存
+    for r := 0; r < 9; r++ {
+        for c:= 0; c < 9; c++ {
+            if board[r][c] != '.' {
+                num := board[r][c] - '0' - byte(1)
+                if rowbuf[r][num] || colbuf[c][num] || boxbuf[r/3*3+c/3][num] {
+                    return false
+                }
+                rowbuf[r][num] = true
+                colbuf[c][num] = true
+                boxbuf[r/3*3+c/3][num] = true
+            }
+        }
+    }
+    return true
+}
