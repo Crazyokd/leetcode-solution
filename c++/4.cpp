411a1a8713fcaceaf90aa4b63bb49bc20d4ffabf
rekord <1324596506@qq.com>
2022-03-02 14:34:20 +0800

    feat: add leetcode-34 solution which use c++

--- c++/4.cpp
+++ c++/4.cpp
@@ -34,6 +34,37 @@ public:
         }
         return -1;
     }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res;
+        // 使用二分找到target
+        int index = getIndex(nums, target);
+        // 对第一个index查找得到结果
+        if (index < 0) {
+            res.push_back(-1);
+            res.push_back(-1);
+        }else {
+            int low = index - 1, high = index + 1;
+            while (low >= 0 && nums[low] == target) low--;
+            while (high < nums.size() && nums[high] == target) high++;
+            res.push_back(low+1);
+            res.push_back(high-1);
+        }
+        return res;
+    }
+    int getIndex(vector<int>& nums, int target) {
+        // 使用二分找到target
+        int low = 0, high = nums.size() - 1;
+        while (high >= low) {
+            int mid = low + ((high-low)>>1);
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) {
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
 };
 
 int main() {
