55a3bae16cdb2e5e380a2a7921dbcaad24d20de0
Rekord <1324596506@qq.com>
2022-03-05 21:17:15 +0800

    feat: add leetcode-39 solution which use go

--- Go/leetcode/4.go
+++ Go/leetcode/4.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "sort"
+
 func search(nums []int, target int) int {
     low, high := 0, len(nums)-1
     for low <= high {
@@ -53,4 +55,33 @@ func isValidSudoku(board [][]byte) bool {
         }
     }
     return true
+}
+
+func combinationSum(candidates []int, target int) [][]int {
+    if len(candidates) == 0 {
+        return [][]int{}
+    }
+    c, res := []int{}, [][]int{}
+    sort.Ints(candidates)
+    findcombinationSum(candidates, target, 0, c, &res)
+    return res
+}
+
+func findcombinationSum(nums []int, target, index int, c []int, res *[][]int) {
+    if target <= 0 {
+        if target == 0 {
+            b := make([]int, len(c))
+            copy(b, c)
+            *res = append(*res, b)
+        }
+        return 
+    }
+    for i := index;i < len(nums); i++ {
+        if nums[i] > target {
+            break
+        }
+        c = append(c, nums[i])
+        findcombinationSum(nums, target-nums[i], i, c, res)
+        c = c[:len(c)-1]
+    }
 }
