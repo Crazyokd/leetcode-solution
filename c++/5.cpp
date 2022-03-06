559a1459b5eed8a96ae2d6711e7ddcea7f7a5d3e
Rekord <1324596506@qq.com>
2022-03-06 13:55:46 +0800

    feat: add leetcode-43 solution which use c++

--- c++/5.cpp
+++ c++/5.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<string>
 
 using namespace std;
 
@@ -13,6 +14,25 @@ public:
             if (abs(nums[i]) <= n && nums[abs(nums[i])-1] > 0)nums[abs(nums[i])-1] *= -1;
         for (int i = 0;i < n;i++)if (nums[i] > 0)return i+1;
         return n+1;
+    }
+
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0")return "0";
+        vector<int> res(num1.size()+num2.size(), 0);
+ 
+        for (int i = num1.size()-1; i >= 0; i--){
+            for (int j = num2.size()-1; j >= 0; j--){
+                res[i+j+1] += (num1[i]-'0')*(num2[j]-'0');
+                res[i+j] += res[i+j+1]/10;
+                res[i+j+1] %= 10;
+            }
+        }
+        int i = 0;
+        string ans = "";
+        while (res[i] == 0) i++;
+        while (i < res.size()) ans += to_string(res[i++]);
+        
+        return ans;
     }
 };
 
