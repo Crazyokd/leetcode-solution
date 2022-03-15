3fae675b630a6bad54448f162aeeb81238a7e2d7
Rekord <1324596506@qq.com>
2022-03-15 14:33:49 +0800

    feat: add leetcode-62 solution which use c++

--- c++/7.cpp
+++ c++/7.cpp
@@ -31,6 +31,33 @@ public:
         tail->next = NULL;
         return head;
     }
+
+    int uniquePaths(int m, int n) {
+        // int res = 0;
+        // uniquePathsHelper(1, 1, m, n, res);
+        // return res;
+        
+        // use dp
+        int arr[101][101] = {0};
+        arr[1][1] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] += arr[i-1][j] + arr[i][j-1];
+            }
+        }
+        return arr[m][n];
+        
+    }
+    
+    // 深搜超时，而且答案也不用求出所有具体路径
+    // void uniquePathsHelper(int row, int col, int m, int n, int& res){
+    //     if (row == m && col == n) {
+    //         res++;
+    //         return;
+    //     }
+    //     if (row < m)uniquePathsHelper(row+1, col, m, n, res);
+    //     if (col < n)uniquePathsHelper(row, col+1, m, n, res);
+    // }
 };
 
 
