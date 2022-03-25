1a92a761a24fdd6668bfb7af01609d2c553bb230
Rekord <1324596506@qq.com>
2022-03-25 17:08:42 +0800

    feat: add leetcode-30 solution which use c++

--- c++/3.cpp
+++ c++/3.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<unordered_map>
 
 using namespace std;
 
@@ -206,9 +207,28 @@ public:
         }
         return lps;
     }
+    
     int divide(int dividend, int divisor) {
         if (dividend == 0x80000000 && divisor == -1)return 0x7FFFFFFF;
         return dividend / divisor;
+    }
+
+    vector<int> findSubstring(string s, vector<string>& words) {
+        // init
+        vector<int> res; 
+        unordered_map<string, int> search;
+        for (auto &word : words) ++search[word]; 
+        int n = s.size(), m = words.size(), len = words[0].size(); // 获取隐藏变量
+        for (int i = 0, j = 0; i <= n - m * len; ++i) {
+            unordered_map<string, int> sub; // 子字符 查找的中间结果
+            for (j = 0; j < m; ++j) {
+                auto word = s.substr(i + j * len, len); // 获取子串
+                if (!search.count(word)) break; // 子串 不在 words 里面
+                if (++sub[word] > search[word]) break; // 子串个数 比 words 多
+            }
+            if (j == m) res.push_back(i); // 完全匹配
+        }
+        return res;
     }
 };
 int main(){
