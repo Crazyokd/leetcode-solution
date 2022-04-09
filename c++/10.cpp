885c3402f3b58a2f829f08f443aa43402235bcc0
Rekord <1324596506@qq.com>
2022-04-09 20:16:22 +0800

    feat: add leetcode-92 solution which use c++

--- c++/10.cpp
+++ c++/10.cpp
@@ -24,6 +24,43 @@ public:
         }
         return res.back();
     }
+
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (head -> next == NULL || left == right) return head;
+        int cnt = 0;
+        ListNode *node_left = new ListNode();
+        node_left -> next = head;
+        while (++cnt < left) {
+            node_left = node_left->next;
+        }
+
+        ListNode *node_right = node_left->next;
+        while (cnt++ <= right) {
+            node_right = node_right->next;
+        }
+
+        ListNode *pre = node_right;
+        ListNode *next = node_left->next;
+        while (next != node_right) {
+            ListNode *tmp = next->next;
+            next -> next = pre;
+            pre = next;
+            next = tmp;
+        }
+
+        bool flag = node_left->next == head; // 判断head节点是否反转
+        node_left->next = pre;
+
+        return flag ? node_left->next : head;
+    }
 };
 
 int main() {
