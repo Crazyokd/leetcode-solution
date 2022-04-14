84074b0c2491ee53a221247b59a5d9b1c609693d
Rekord <1324596506@qq.com>
2022-04-14 19:25:54 +0800

    feat: add leetcode-1672 solution which use c++

---
+++ c++/168.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int res = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].size(); j++) {
+                sum += accounts[i][j]; 
+            }
+            res = max(res, sum);
+        }
+        return res;
+    }
+};
+
+int main() {
+    return 0;
+}
