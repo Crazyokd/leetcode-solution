7aea8d7616bcaf207d8f0bf4005c6b206bb9171c
Rekord <1324596506@qq.com>
2022-04-14 20:18:37 +0800

    feat: add leetcode-103 and leetcode-104 solution which all use c++

--- c++/11.cpp
+++ c++/11.cpp
@@ -14,7 +14,7 @@ public:
         TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
         TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
     };
-    
+
     bool isSymmetric(TreeNode* root) {
         return !root || helper(root->left,root->right);
     }
@@ -54,8 +54,54 @@ public:
         }
         return res;
     }
+
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (root == NULL) return res;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        bool traversal_order = false;
+        int lay_cnt = 1;
+        while(!q.empty()) {
+            vector<int> layer_res;
+            int new_lay_cnt = 0;
+            for (int i = 0; i < lay_cnt; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                layer_res.emplace_back(node->val);
+                if (node->left != NULL) {
+                    q.push(node->left);
+                    new_lay_cnt++;
+                }
+                if (node->right != NULL) {
+                    q.push(node->right);
+                    new_lay_cnt++;
+                }
+            }
+            lay_cnt = new_lay_cnt;
+            if (traversal_order) {
+                int len = layer_res.size();
+                vector<int> reverse_layer_res(len);
+                for (int i = 0; i < len; i++) {
+                    reverse_layer_res[len-i-1] = layer_res[i];
+                }
+                res.emplace_back(reverse_layer_res);
+            } else {
+                res.emplace_back(layer_res);
+            }
+            traversal_order = traversal_order ? false : true;   
+        }
+        return res;
+    }
+
+    int maxDepth(TreeNode* root) {
+        return root?max(maxDepth(root->left),maxDepth(root->right))+1:0;
+    }    
 };
 
 int main() {
+    vector<int> nums;
     return 0;
 }
