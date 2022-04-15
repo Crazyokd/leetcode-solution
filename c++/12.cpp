3401ff89aad7563e9f43cf9793aeada3d7db70bd
Rekord <1324596506@qq.com>
2022-04-15 10:51:24 +0800

    feat: add leetcode-114 solution which use c++

--- c++/12.cpp
+++ c++/12.cpp
@@ -4,16 +4,17 @@
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    struct TreeNode {
-        int val;
-        TreeNode *left;
-        TreeNode *right;
-        TreeNode() : val(0), left(nullptr), right(nullptr) {}
-        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-    };
 
     int minDepth(TreeNode* root) {
         if (root == NULL) return 0;
@@ -68,8 +69,38 @@ public:
         getAllPathSum(root->left, curSum, targetSum, curPath, res);
         getAllPathSum(root->right, curSum, targetSum, curPath, res);
     }
+
+    void flatten(TreeNode* root) {
+        if (root == NULL) return;
+        flattenHelper(root);
+    }
+    TreeNode* flattenHelper(TreeNode* root) {
+        TreeNode *right = root->right;
+        root->right = root->left;
+        root->left = NULL;
+
+        if (root->right != NULL) {
+            root = flattenHelper(root->right);
+        }
+        root->right = right;
+        if (right != NULL) {
+            root = flattenHelper(root->right);
+        }
+        return root;
+    }
 };
 
 int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+
+    root->right = new TreeNode(5);
+    root->right->right = new TreeNode(6);
+
+    Solution s;
+    s.flatten(root);
+
     return 0;
 }
