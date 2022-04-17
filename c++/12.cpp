f4809abe9d426424b46cb837812eeb8669ef657d
Rekord <1324596506@qq.com>
2022-04-17 15:29:08 +0800

    feat: add leetcode-118 solution which use c++

--- c++/12.cpp
+++ c++/12.cpp
@@ -204,6 +204,19 @@ public:
         }
         return root;        
     }
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res(numRows);
+
+        for (int i = 1; i <= numRows; i++) {
+            vector<int> layer(i, 1);
+            for (int j = 1; j < layer.size()-1; j++) {
+                layer[j] = res[i-2][j-1] + res[i-2][j];
+            } 
+            res[i-1] = layer;           
+        }
+        return res;
+    }
 };
 
 int main() {
