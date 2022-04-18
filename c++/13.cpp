fcb4f6ebb663673b052ad0de084a2d2f9806b18c
Rekord <1324596506@qq.com>
2022-04-18 20:54:09 +0800

    feat: add leetcode-122 solution which use c++

--- c++/13.cpp
+++ c++/13.cpp
@@ -15,6 +15,20 @@ public:
         }
         return res;
     }
+
+    int maxProfit(vector<int>& prices) {
+        int minn = prices[0];
+        int res = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > minn) {
+                res += prices[i] - minn;
+                minn = prices[i];
+            } else {
+                minn = min(prices[i], minn);
+            }
+        }
+        return res;
+    }
 };
 
 int mian() {
