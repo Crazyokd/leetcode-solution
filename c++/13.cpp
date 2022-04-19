ce52e610bd25ce9aeec832ee6ed6b8a842a38b58
Rekord <1324596506@qq.com>
2022-04-19 15:18:39 +0800

    feat: add leetcode-123 solution which use c++

--- c++/13.cpp
+++ c++/13.cpp
@@ -16,7 +16,7 @@ public:
         return res;
     }
 
-    int maxProfit(vector<int>& prices) {
+    int maxProfitII(vector<int>& prices) {
         int minn = prices[0];
         int res = 0;
         for (int i = 1; i < prices.size(); i++) {
@@ -29,8 +29,25 @@ public:
         }
         return res;
     }
+
+    // https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/solution/mai-mai-gu-piao-de-zui-jia-shi-ji-iii-by-wrnt/
+    int maxProfitIII(vector<int>& prices) {
+        int n = prices.size();
+        int buy1 = -prices[0], sell1 = 0;
+        int buy2 = -prices[0], sell2 = 0;
+        for (int i = 1; i < n; ++i) {
+            buy1 = max(buy1, -prices[i]);
+            sell1 = max(sell1, buy1 + prices[i]);
+            buy2 = max(buy2, sell1 - prices[i]);
+            sell2 = max(sell2, buy2 + prices[i]);
+        }
+        return sell2;
+    }
 };
 
-int mian() {
+int main() {
+    Solution s;
+    vector<int> prices = {3,3,5,0,0,3,1,4};
+    s.maxProfitIII(prices);
     return 0;
 }
