19f17e26580544ec00f85099ae851ff48742375c
Rekord <1324596506@qq.com>
2022-04-20 20:14:13 +0800

    feat: add leetcode-388 solution which use c++

--- c++/39.cpp
+++ c++/39.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<string>
+#include<stack>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -78,8 +80,52 @@ public:
             DFS(cur*10+i, n);
         }
     }
+
+    // https://leetcode-cn.com/problems/longest-absolute-file-path/solution/wen-jian-de-zui-chang-jue-dui-lu-jing-by-fi0r/
+    int lengthLongestPath(string input) {
+        int n = input.size();
+        int pos = 0;
+        int ans = 0;
+        stack<int> st;
+
+        while (pos < n) {
+            /* 检测当前文件的深度 */
+            int depth = 1;
+            while (pos < n && input[pos] == '\t') {
+                pos++;
+                depth++;
+            }
+            /* 统计当前文件名的长度 */   
+            int len = 0; 
+            bool isFile = false;     
+            while (pos < n && input[pos] != '\n') {
+                if (input[pos] == '.') {
+                    isFile = true;
+                }
+                len++;
+                pos++;
+            }
+            /* 跳过换行符 */
+            pos++;
+
+            while (st.size() >= depth) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                len += st.top() + 1;
+            }
+            if (isFile) {
+                ans = max(ans, len);
+            } else {
+                st.emplace(len);
+            }
+        }
+        return ans;
+    }    
 };
 
 int main() {
+    Solution s;
+    s.lengthLongestPath("dir\\n\\tsubdir1\\n\\tsubdir2\\n\\t\\tfile.ext");
     return 0;
 }
