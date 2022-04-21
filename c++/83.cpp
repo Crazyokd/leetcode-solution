988965170927f5f56530a3fa410c9ba86cd5539b
Rekord <1324596506@qq.com>
2022-04-21 17:36:25 +0800

    feat: add leetcode-824 solution which use c++

--- c++/83.cpp
+++ c++/83.cpp
@@ -21,6 +21,26 @@ public:
         }
         return res;
     }
+
+    string toGoatLatin(string sentence) {
+        sentence += " ";
+        string res = "";
+        for (int i = 0, cnt = 0; i < sentence.size(); i++) {
+            string::iterator isFindElement = find(sentence.begin()+i, sentence.end(), ' ');
+            if (isFindElement != sentence.end()) {
+                int len = isFindElement - sentence.begin() - i;
+                string word = sentence.substr(i, len);
+                char fc = word[0] < 'a' ? word[0]+32 : word[0];
+                if (!(fc == 'a' || fc == 'e' || fc == 'i' || fc == 'o' || fc == 'u')) {
+                    word = word.substr(1) + word[0];
+                }
+                res += word + "ma" + string(cnt+1, 'a') + " ";
+                i += len;
+                cnt++;
+            }
+        }
+        return res.substr(0, res.size()-1);
+    }
 };
 
 int main() {
