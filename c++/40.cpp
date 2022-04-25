ec597741b9ac8c42e7b03b095dfa9dfdd4a1ff0f
Rekord <1324596506@qq.com>
2022-04-25 19:14:12 +0800

    feat: add leetcode-398 solution which use c++

--- c++/40.cpp
+++ c++/40.cpp
@@ -2,8 +2,24 @@
 #include<algorithm>
 #include<vector>
 #include<numeric>
+#include<unordered_map>
 
 using namespace std;
+
+class Solution {
+    unordered_map<int, vector<int>> pos;
+public:
+    Solution(vector<int> &nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            pos[nums[i]].push_back(i);
+        }
+    }
+
+    int pick(int target) {
+        auto &indices = pos[target];
+        return indices[rand() % indices.size()];
+    }
+};
 
 class Solution {
 public:
