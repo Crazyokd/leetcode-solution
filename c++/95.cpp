ef7a14c41d0a54d356412f9f8d3f45eee1f06fe6
Rekord <1324596506@qq.com>
2022-05-09 20:19:03 +0800

    feat: add leetcode-942 solution which use c++

---
+++ c++/95.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int n = s.length(), lo = 0, hi = n;
+        vector<int> perm(n + 1);
+        for (int i = 0; i < n; ++i) {
+            perm[i] = s[i] == 'I' ? lo++ : hi--;
+        }
+        perm[n] = lo; // 最后剩下一个数，此时 lo == hi
+        return perm;
+    }
+};
+
+int main() {
+    return 0;
+}
