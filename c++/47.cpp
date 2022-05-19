3aed88651fec391a02aba490ce72454c6f9e5409
Rekord <1324596506@qq.com>
2022-05-19 20:16:50 +0800

    feat: add leetcode-462 solution which use c++

--- c++/47.cpp
+++ c++/47.cpp
@@ -1,9 +1,38 @@
 #include<iostream>
 #include<cmath>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 class Solution {
 public:
+    int minMoves21(vector<int>& nums) {
+        // 将数组排序
+        sort(nums.begin(), nums.end());
+        // 取中位数
+        int average = nums[nums.size()/2];
+        int res = 0;
+        for (const int &num : nums) {
+            res += abs(average - num);
+        }
+        return res;
+    }
+        int minMoves22(vector<int>& nums) {
+        // 将数组排序
+        sort(nums.begin(), nums.end());
+        int size = nums.size();
+        // 取中位数
+        int average = nums[size/2];
+        int res = 0;
+        // for (const int &num : nums) {
+        //     res += abs(average - num);
+        // }
+        for (int i = 0; i < size/2; i++) {
+            res += nums[size-i-1] - nums[i];
+        }
+        return res;
+    }
+
     int largestPalindrome(int n) {
         if (n == 1) return 9;
         int upper = pow(10, n);
